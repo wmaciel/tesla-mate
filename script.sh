5a3045739eb97e22b853751000e719ec79314700
Walther Maciel <walthermaciel@gmail.com>
2021-06-13 17:41:36 -0700

    points script.sh to forked repo

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ sudo usermod -a -G docker ec2-user
 sudo docker info
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/jonatannietoa/tesla-mate.git
+git clone https://github.com/wmaciel/tesla-mate.git
